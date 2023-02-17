db8c02602436644d74a9b0ea228987245860d455
Ephraim Gathoni <ephnjor@gmail.com>
2023-02-17 15:07:54 -0800

    6-print-numberz.c

---
+++ 0x01-variables_if_else_while/6-print-numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+    int i = 0;
+
+    while (i < 10) {
+        putchar('0'+i);
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
