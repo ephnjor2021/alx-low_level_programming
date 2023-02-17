d4376f2758fd3e9d28eb35f82b2c388f5e6d21c5
Ephraim Gathoni <ephnjor@gmail.com>
2023-02-17 15:13:31 -0800

    6-print-numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-    int i = 0;
-
-    while (i < 10) {
-        putchar('0'+i);
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	int i = 0;
+	
+	while (i < 10) 
+	{
+	putchar('0'+i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
 }
