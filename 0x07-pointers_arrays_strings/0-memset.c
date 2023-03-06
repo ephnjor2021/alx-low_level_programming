f35e3d0d04328c75a6e327e749044b1c117a0cab
Ephraim Gathoni <ephnjor@gmail.com>
2023-03-06 13:31:17 -0800

    update task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,13 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _memset - sets memory to array
- * @s: array to set
- * @b: value to set it as
- * @n: n amount of times
- * Return: char value of s
+ * _memset - fills memory with a constant byte
+ * @s: string array
+ * @b: constant byte
+ * @n: number of bytes
+ * Return: pointer
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -14,5 +16,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
