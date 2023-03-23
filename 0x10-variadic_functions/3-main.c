5d2a3fd6d54dc59baff0f5c2ee8e3e6ae0dee321
Ephraim Gathoni <ephnjor@gmail.com>
2023-03-23 10:47:14 -0700

    task 3

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'B', 3, "stSchool");
+	print_all(NULL);
+	return (0);
+}
