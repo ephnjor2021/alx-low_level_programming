2e13bcf2b68abe07a1b4f61edce6f367481a60ab
Ephraim <ephnjor@gmail.com>
2023-09-14 09:52:39 -0700

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,46 +1,62 @@
 #include "search_algos.h"
-int minimum(int left, int right);
-/**
-* jump_search - searches for a value in a sorted array
-* of integers using jump search algorithm
-* @array: is a pointer to the first element of the array to search in
-* @size: size of the array
-* @value: is the value to search for
-* Return: If value is not present in array return -1
-*/
-int jump_search(int *array, size_t size, int value)
-{
-	int prev = 0, jump = sqrt((int)size), pos = 0;
+#include <math.h>
 
-	if (array == NULL)
-		return (-1);
-	for (; prev <= (int)size - 1; prev += jump)
+/**
+ * chck_sarr - checks a subarray
+ * @array: pointer to array
+ * @size: size of array
+ * @first: index of first element to print
+ * @last: index of last element to print
+ * @val: value to match
+ * Return: index of match or -1 if no match
+ */
+
+int chck_sarr(int *array, size_t size, size_t first, size_t last, int val)
+{
+	size_t i = 0;
+
+	for (i = first; i <= last && i < size; i++)
 	{
-		if (array[prev] >= value)
-			break;
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-	}
-	pos = prev - jump;
-	printf("Value found between indexes [%d] and [%d]\n", pos, prev);
-	for (; pos <= prev; pos++)
-	{
-		if (pos < (int)size)
-		{
-			printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
-			if (array[pos] == value)
-				return (pos);
-		}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == val)
+			return (i);
 	}
 	return (-1);
 }
 
 /**
-* minimum - return the minimum of two numbers
-* @left: first number
-* @right:second number
-* Return: Return the minimum
-*/
-int minimum(int left, int right)
+ * jump_search - search for value in sorted int arr using jump search algo
+ * @array: pointer to array
+ * @size: size of array
+ * @value: value to search
+ * Return: index of match or -1 if no match
+ */
+
+int jump_search(int *array, size_t size, int value)
 {
-	return (left <= right ? left : right);
+	size_t interval = 0;
+	size_t i = 0;
+	char *message = "Value found between indexes ";
+
+	if (array == NULL || size < 1)
+		return (-1);
+
+	interval = sqrt(size);
+	for (i = 0; i < size; i += interval)
+	{
+		if (array[i] == value)
+		{
+			printf("%s[%lu] and [%lu]\n", message, i - interval, i);
+			return (chck_sarr(array, size, i - interval, i, value));
+		}
+		else if (value < array[i])
+		{
+			printf("%s[%lu] and [%lu]\n", message, i - interval, i);
+			return (chck_sarr(array, size, i - interval, i, value));
+		}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+	}
+	printf("%s[%lu] and [%lu]\n", message, i - interval, i);
+	return (chck_sarr(array, size, i - interval, i, value));
 }
