74ce17d0605321fd0771c80faa7d13ecd8ff64d6
Ephraim <ephnjor@gmail.com>
2023-10-22 09:50:41 -0700

    update search algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,14 +1,17 @@
-#ifndef _SEARCH_H_
-#define _SEARCH_H_
-
-/** HEADERS */
-
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-/** Prototype */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+int bs_helper2(int *array, int key, int low, int high);
+int bs_helper(int *array, int key, int lower, int higher);
+int helper(int *array, int key, int low, int high);
+void helper2(int *array, int low, int high);
+int jump_search(int *array, size_t size, int value);
+int min(int a, int b);
 #endif
